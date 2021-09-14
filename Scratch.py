a91991c01be3f78c34b49427449ccd9a35171cb2
Jimmy Johnson <89602279+ChipotleMuncher@users.noreply.github.com>
2021-09-14 13:44:32 -0400

    Update Scratch.py

--- Scratch.py
+++ Scratch.py
@@ -1,6 +1,19 @@
-from os import system
-def cls():
-    system ('cls')
+logged_in = False
+attempts = 2
 
-print('hello\n'*200)
-cls()
+while not logged_in:
+    print (f'You have {attempts+1} attempts remaining.')
+    username = input ('Username: ')
+    password = input ('Password: ')
+
+    if username == 'user' and password == 'pass':
+        print ('Logged in!')
+        logged_in = True
+    elif attempts > 0:
+        attempts = attempts - 1
+        print ('Log in failed, try again.')
+    else:
+        print ('You have exceeded your attempts.')
+        break
+
+print ('after loop')
