e9bec2426a005128df07fb1ecbb07e566f202379
Jimmy Johnson <89602279+ChipotleMuncher@users.noreply.github.com>
2021-10-05 13:37:08 -0400

    Update Scratch.py

--- Scratch.py
+++ Scratch.py
@@ -1,19 +1,23 @@
-logged_in = False
-attempts = 2
+# logged_in = False
+# attempts = 2
 
-while not logged_in:
-    print (f'You have {attempts+1} attempts remaining.')
-    username = input ('Username: ')
-    password = input ('Password: ')
+# while not logged_in:
+#     print (f'You have {attempts+1} attempts remaining.')
+#     username = input ('Username: ')
+#     password = input ('Password: ')
 
-    if username == 'user' and password == 'pass':
-        print ('Logged in!')
-        logged_in = True
-    elif attempts > 0:
-        attempts = attempts - 1
-        print ('Log in failed, try again.')
-    else:
-        print ('You have exceeded your attempts.')
-        break
+#     if username == 'user' and password == 'pass':
+#         print ('Logged in!')
+#         logged_in = True
+#     elif attempts > 0:
+#         attempts = attempts - 1
+#         print ('Log in failed, try again.')
+#     else:
+#         print ('You have exceeded your attempts.')
+#         break
 
-print ('after loop')
+# print ('after loop')
+
+
+
+print ('Hello\n'*50)
